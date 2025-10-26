965127f3a423295d9dca170ec88b958c21e81f32
whiskend <lsg008@naver.com>
2025-10-26 20:59:20 +0900

    feat: 자동차 이름 형식 검증 추가(1~5자 && 빈값 && 공백 불가)
     - README 2번 섹션
    
     - 사용자가 입력한 자동차 이름의 형식이 올바르지 않으면 IllegalArgumentException을 발생-m     - 이름이 1~5자가 아니거나 trim() 결과가 빈 문자열이거나 null 또는 빈 입력인 경우 발생-m - InputView 내부에 validateCarNames() 메서드 추가-m - Application에서 parse 후 validate 순서로 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,6 +11,7 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = Console.readLine();
         List<String> carNames = InputView.parseCarNames(carNamesInput);
+        InputView.validateCarNames(carNames);
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         String numberOccurrences = Console.readLine();
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,7 +7,19 @@ import java.util.stream.Collectors;
 public class InputView {
     private InputView(){
     }
+
     public static List<String> parseCarNames(String input) {
         return Arrays.stream(input.split(",")).map(String::trim).collect(Collectors.toList());
+    }
+
+    public static void validateCarNames(List<String> carNames) {
+        for (String name : carNames) {
+            if (name == null || name.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 비어 있을 . 없습니다.");
+            }
+            if (name.length() < 1 || name.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+            }
+        }
     }
 }
